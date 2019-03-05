8cdb35cc4896784875bc97b54131ea2c9c109aa1
Joris <j.fabre@rubika-edu.com>
2019-03-05 17:46:30 +0100

    Plus de travail

---
+++ Assets/ChooseColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseColor : MonoBehaviour
+{
+    public Material selectedColor;
+    public List<Material> Colors;
+    public int index = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = Random.Range(0, Colors.Count+1);
+        Debug.Log(index);
+        selectedColor = Colors[index];
+        gameObject.GetComponent<Renderer>().material = selectedColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ColorFormula.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFormula : MonoBehaviour
+{ 
+public static ColorFormula staticColorFormula;
+public bool firstColor = false;
+public bool secondColor = false;
+public bool thirdColor = false;
+public bool fourthColor = false;
+
+public void Awake()
+{
+        staticColorFormula = this;
+}
+
+public void FixedUpdate()
+{
+    KeysDown();
+
+    KeysUp();
+}
+
+public void KeysDown()
+{
+    if (Input.GetKeyDown(KeyCode.Alpha1) && !firstColor)
+    {
+        firstColor = !firstColor;
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha2) && !secondColor)
+    {
+        secondColor = !secondColor;
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha3) && !thirdColor)
+    {
+        thirdColor = !thirdColor;
+    }
+    else if (Input.GetKeyDown(KeyCode.Alpha4) && !fourthColor)
+    {
+        fourthColor = !fourthColor;
+    }
+}
+
+public void KeysUp()
+{
+    if (Input.GetKeyUp(KeyCode.Alpha1) && firstColor)
+    {
+        firstColor = !firstColor;
+    }
+    else if (Input.GetKeyUp(KeyCode.Alpha2) && secondColor)
+    {
+        secondColor = !secondColor;
+    }
+    else if (Input.GetKeyUp(KeyCode.Alpha3) && thirdColor)
+    {
+        thirdColor = !thirdColor;
+    }
+    else if (Input.GetKeyUp(KeyCode.Alpha4) && fourthColor)
+    {
+        fourthColor = !fourthColor;
+    }
+}
+}
